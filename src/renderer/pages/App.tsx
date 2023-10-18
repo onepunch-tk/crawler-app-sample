d91b1817ee326555e4033310c5590fd46f07e3fe
TaekyungHa <86tkstar@gmail.com>
2023-10-19 03:11:21 +0900

    add toggle button and SVG icons

--- src/renderer/pages/App.tsx
+++ src/renderer/pages/App.tsx
@@ -15,8 +15,8 @@ export function App() {
   return (
     <Fragment>
       <div className="flex mt-2 ml-2">
-        <MoonSvg />
-        <label className="relative inline-flex items-center mx-2 cursor-pointer">
+        <SunSvg />
+        <label className="relative inline-flex items-center mx-3 cursor-pointer">
           <input
             onChange={toggleHandler}
             type="checkbox"
@@ -24,9 +24,9 @@ export function App() {
             className="sr-only peer"
             checked={isDark}
           />
-          <div className="w-9 h-5 bg-gray-200 rounded-full peer peer-focus:ring-2 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-300 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-red-600"></div>
+          <div className="w-[2.5rem] h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-1 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-[calc(2.5rem-1rem-4px)] peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-[1rem] after:w-[1rem] after:transition-all dark:border-gray-600 peer-checked:bg-blue-600" />
         </label>
-        <SunSvg />
+        <MoonSvg />
       </div>
     </Fragment>
   );
