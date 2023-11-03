96b67fd96598155381cb1a7145b9e38c5259e882
TaekyungHa <86tkstar@gmail.com>
2023-11-03 14:40:55 +0900

    instagram-page-search

--- src/renderer/components/section-card/CardInputWrapper.tsx
+++ src/renderer/components/section-card/CardInputWrapper.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from "react";
 
 export function CardInputWrapper({ children }: PropsWithChildren) {
   return (
-    <div className="flex w-full justify-items-start items-center h-5 mb-5">
+    <div className="flex w-full justify-items-start items-center h-5 mb-7">
       {children}
     </div>
   );
--- src/renderer/components/section-card/CardWrapper.tsx
+++ src/renderer/components/section-card/CardWrapper.tsx
@@ -1,8 +1,12 @@
 import { PropsWithChildren } from "react";
 
+// type CardWrapperProps = {
+//   height:
+// }
+
 export function CardWrapper({ children }: PropsWithChildren) {
   return (
-    <section className="flex-box-col-center box-light-bg dark:box-dark-bg w-1/2 min-w-[350px] rounded-md p-10 shadow-box dark:shadow-box-dark transition-[border-color,box-shadow] duration-300 h-40">
+    <section className="flex-box-col-center box-light-bg dark:box-dark-bg w-1/2 min-w-[350px] rounded-md p-10 shadow-box dark:shadow-box-dark transition-[border-color,box-shadow] duration-300">
       {children}
     </section>
   );
